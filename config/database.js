cb92008dd990392cc6ae5c8358a5e40fdb4d4449
Alicia Mira Kim <alicia.mira.kim@gmail.com>
2020-10-30 18:29:26 -0400

    Fixed database url again.

--- config/database.js
+++ config/database.js
@@ -15,7 +15,7 @@ module.exports = {
     dialect: 'postgres',
   },
   production: {
-    use_env_variable: 'postgres://mqbqkpbgwcyyqa:25f088cb7af99fb4d77b3070ab8c5ceeb2428f480abfdc7835912fc59158c2f4@ec2-50-17-197-184.compute-1.amazonaws.com:5432/df8at2nbq1q82e',
+    use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
     seederStorage: 'sequelize',
   }
