1e7fd116421ecb6111345cd22ca6f9bd4e33f5e6
Alicia Mira Kim <alicia.mira.kim@gmail.com>
2020-10-30 18:17:36 -0400

    Fixed the database url in config.

--- config/database.js
+++ config/database.js
@@ -13,10 +13,9 @@ module.exports = {
     database,
     host,
     dialect: 'postgres',
-    seederStorage: 'sequelize',
   },
   production: {
-    use_env_variable: 'DATABASE_URL',
+    use_env_variable: 'postgres://mqbqkpbgwcyyqa:25f088cb7af99fb4d77b3070ab8c5ceeb2428f480abfdc7835912fc59158c2f4@ec2-50-17-197-184.compute-1.amazonaws.com:5432/df8at2nbq1q82e',
     dialect: 'postgres',
     seederStorage: 'sequelize',
   }
