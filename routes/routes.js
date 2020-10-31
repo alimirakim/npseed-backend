1ac4da46842a562656f5886ba9f097e1433f45ed
Alicia Mira Kim <alicia.mira.kim@gmail.com>
2020-10-30 23:31:58 -0400

    Just tweaking.

--- routes/routes.js
+++ routes/routes.js
@@ -5,18 +5,13 @@ router.get('/', (req, res) => {
   res.redirect("/")
 })
 
-
-// router.get("/users/:id/characters",
-//   async (req, res) => {
-//   const userChars = await Character.findAll({
-//     where: { userId },
-//     include: [Trait, TraitOption],
-//   })
-//   res.json(userChars)
-// }
-
-
-
-
+// Fetch array of User's Chars
+router.get("/users/:id/characters", async (req, res) => {
+  const userChars = await Character.findAll({
+    where: { userId },
+    include: [Trait, TraitOption],
+  })
+  res.json(userChars)
+})
 
 module.exports = router
